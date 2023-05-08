20e7405367c3afa6663367648ce5284d3fe5770d
Vlads Muravjovs <vlads.muravjovs@gmail.com>
2023-05-08 09:11:20 +0300

    Project deletion action

---
+++ actions/deleteproject.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+include_once('../utils/sqlutils.php');
+use Utils\SQLFormatter;
+
+include_once('../utils/connect.php');
+
+$formatter = new SQLFormatter();
+
+//get milestones that align with the project id
+$sql = "SELECT id FROM milestones WHERE project_id=" . $_GET['id'];
+$result = $conn->query($sql);
+
+$dataset = $result->fetch_array(MYSQLI_NUM);
+
+if($dataset !== null){
+    //delete all linked assignments
+    for($i=0;$i<count($dataset);$i++){
+        $sql = "DELETE FROM assignments WHERE milestone_id=" . $dataset[$i];
+        $result = $conn->query($sql);
+    }
+}
+
+//delete all milestones
+$sql = "DELETE FROM milestones WHERE project_id=" . $_GET['id'];
+$result = $conn->query($sql);
+
+//delete the project
+$sql = "DELETE FROM projects WHERE id=" . $_GET['id'];
+$result = $conn->query($sql);
+
+echo json_encode($result);
