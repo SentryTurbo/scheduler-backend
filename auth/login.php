937f6c472333ebfa85a5d985d5df059c3e16b21f
Vlads Muravjovs <vlads.muravjovs@gmail.com>
2023-05-10 10:41:17 +0300

    Auth token

--- auth/login.php
+++ auth/login.php
@@ -5,15 +5,23 @@ use Utils\SQLFormatter;
 
 include_once('../utils/headers.php');
 include_once('../utils/connect.php');
+include_once('../utils/encrypt.php');
 
 $formatter = new SQLFormatter();
-$sql = "SELECT id,pass FROM users WHERE 'username'='". $_POST['username'] . "'";
+$sql = "SELECT id,pass FROM users WHERE username='". $_POST['username'] . "'";
 
 $result = $conn->query($sql);
 
 $user = $result->fetch_assoc();
+if(is_null($user))
+    die('ERROR');
 
 //check password
 $loginResult = password_verify($_POST['pass'], $user['pass']);
 
-echo json_encode($loginResult);
+if($loginResult === true){
+    $encryptor = new EncryptorBasic();
+    echo json_encode([$_POST['username'], $encryptor->encrypt($_POST['pass'])]);    
+}else{
+    die("ERROR");
+}
---
+++ utils/encrypt.php
@@ -0,0 +1,41 @@
+<?php
+
+class EncryptorBasic{
+    public function encrypt($string){
+        // Store the cipher method
+        $ciphering = "AES-128-CTR";
+        
+        // Use OpenSSl Encryption method
+        $iv_length = openssl_cipher_iv_length($ciphering);
+        $options = 0;
+        
+        // Non-NULL Initialization Vector for encryption
+        $encryption_iv = '1234567891011121';
+        
+        // Store the encryption key
+        $encryption_key = "SchedulerSweetCakes";
+
+        $encryption = openssl_encrypt($string, $ciphering,
+                    $encryption_key, $options, $encryption_iv);
+        
+        return $encryption;
+    }
+
+    public function decrypt($string){
+        
+        // Store the cipher method
+        $ciphering = "AES-128-CTR";
+
+        $decryption_iv = '1234567891011121';
+        $options = 0;
+  
+        // Store the decryption key
+        $decryption_key = "SchedulerSweetCakes";
+        
+        // Use openssl_decrypt() function to decrypt the data
+        $decryption=openssl_decrypt ($string, $ciphering, 
+                $decryption_key, $options, $decryption_iv);
+
+        return $decryption;
+    }
+}
